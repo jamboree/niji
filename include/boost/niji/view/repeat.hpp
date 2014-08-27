5daf2fe776c346c3e9d99bf65d31e116c0538a04
Jamboree <tongari95@gmail.com>
2014-08-27 23:49:36 +0800

    fix repeat

--- include/boost/niji/view/repeat.hpp
+++ include/boost/niji/view/repeat.hpp
@@ -12,42 +12,39 @@
 #include <boost/niji/view/translate.hpp>
 #include <boost/niji/support/traits.hpp>
 #include <boost/niji/support/view.hpp>
+#include <boost/niji/support/utility/as.hpp>
 
 namespace boost { namespace niji
 {
-    struct repeat_view : view<repeat_view>
+    template<class T>
+    struct repeat_view : view<repeat_view<T>>
     {
         template<class Path, class...>
         using point_type = point<path_coordinate_t<Path>>;
-                
-        template<class Path, class Sink, class T>
-        static void iterate(Path const& path, Sink& sink, std::size_t n, vector<T> const& v)
-        {
-             iterate_1d(path, sink, n, v.x, v.y, views::identity);
-        }
         
-        template<class Path, class Sink, class T>
-        static void iterate(Path const& path, Sink& sink, std::size_t nx, std::size_t ny, T dx, T dy)
-        {
-             iterate_2d(path, sink, nx, ny, dx, dy, views::identity);
-        }
-                
-        template<class Path, class Sink, class T>
-        static void reverse_iterate(Path const& path, Sink& sink, std::size_t n, vector<T> const& v)
-        {
-             iterate_1d(path, sink, n, v.x, v.y, views::reverse);
-        }
+        std::size_t n;
+        vector<T> v;
         
-        template<class Path, class Sink, class T>
-        static void reverse_iterate(Path const& path, Sink& sink, std::size_t nx, std::size_t ny, T dx, T dy)
+        repeat_view(std::size_t n, vector<T> const& v)
+          : n(n), v(v)
+        {}
+   
+        template<class Path, class Sink>
+        void iterate(Path const& path, Sink& sink) const
         {
-             iterate_2d(path, sink, nx, ny, dx, dy, views::reverse);
+             iterate_impl(path, sink, n, v.x, v.y, views::identity);
         }
-        
+  
+        template<class Path, class Sink>
+        void reverse_iterate(Path const& path, Sink& sink) const
+        {
+             iterate_impl(path, sink, n, v.x, v.y, views::reverse);
+        }
+
     private:
         
-        template<class Path, class Sink, class T, class View>
-        static void iterate_1d(Path const& path, Sink& sink, std::size_t n, T dx, T dy, View view)
+        template<class Path, class Sink, class View>
+        static void iterate_impl(Path const& path, Sink& sink, std::size_t n, T dx, T dy, View view)
         {
             using coord_t = path_coordinate_t<Path>;
             
@@ -58,13 +55,41 @@ namespace boost { namespace niji
             for (std::size_t i = 0; i != n; ++i)
             {
                 transformed.iterate(sink);
-                translate.dx += dx;
-                translate.dy += dy;
+                translate.tx += dx;
+                translate.ty += dy;
             }
         }
+    };
+    
+    template<class T>
+    struct repeat2_view : view<repeat2_view<T>>
+    {
+        template<class Path, class...>
+        using point_type = point<path_coordinate_t<Path>>;
         
-        template<class Path, class Sink, class T, class View>
-        static void iterate_2d(Path const& path, Sink& sink, std::size_t nx, std::size_t ny, T dx, T dy, View view)
+        std::size_t nx, ny;
+        T dx, dy;
+        
+        repeat2_view(std::size_t nx, std::size_t ny, T dx, T dy)
+          : nx(nx), ny(ny), dx(dx), dy(dy)
+        {}
+
+        template<class Path, class Sink>
+        void iterate(Path const& path, Sink& sink) const
+        {
+             iterate_impl(path, sink, nx, ny, dx, dy, views::identity);
+        }
+
+        template<class Path, class Sink>
+        void reverse_iterate(Path const& path, Sink& sink) const
+        {
+             iterate_impl(path, sink, nx, ny, dx, dy, views::reverse);
+        }
+        
+    private:
+
+        template<class Path, class Sink, class View>
+        static void iterate_impl(Path const& path, Sink& sink, std::size_t nx, std::size_t ny, T dx, T dy, View view)
         {
             using coord_t = path_coordinate_t<Path>;
             
@@ -77,10 +102,10 @@ namespace boost { namespace niji
                 for (std::size_t j = 0; j != nx; ++j)
                 {
                     transformed.iterate(sink);
-                    translate.dx += dx;
+                    translate.tx += dx;
                 }
-                translate.dx = 0;
-                translate.dy += dy;
+                translate.tx = 0;
+                translate.ty += dy;
             }
         }
     };
@@ -88,7 +113,17 @@ namespace boost { namespace niji
 
 namespace boost { namespace niji { namespace views
 {
-    constexpr repeat_view repeat {};
+    template<class T>
+    inline repeat_view<T> repeat(std::size_t n, vector<as_t<T>> const& v)
+    {
+        return {n, v};
+    }
+    
+    template<class T>
+    inline repeat2_view<T> repeat(std::size_t nx, std::size_t ny, as_t<T> dx, as_t<T> dy)
+    {
+        return {nx, ny, dx, dy};
+    }
 }}}
 
 #endif
